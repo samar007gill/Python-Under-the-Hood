01839847d331b90881567cce18e19c9e10ceae66
SAMAR GILL <gillsamar087@gmail.com>
2025-02-28 09:55:21 -0800

    Update main.py

--- 01_Python_fundamentals/03_Character_sets/main.py
+++ 01_Python_fundamentals/03_Character_sets/main.py
@@ -1,1 +1,8 @@
+# -*- coding: utf-8 -*-  
+# This ensures Python correctly interprets non-ASCII characters (like Japanese)
 
+# Define a Japanese greeting
+greeting = "おはよう, みんな!"  # "Good morning, everyone!" in Japanese
+
+# Print the greeting message
+print(greeting)
